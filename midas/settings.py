df46e7f193a967046f9b004934b9eb28830ba0a8
Lionel Panhaleux <lionel.panhaleux@polyconseil.fr>
2018-10-25 16:28:32 +0200

    settings: use Django template, use apps.py

--- midas/settings.py
+++ midas/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("MIDAS_DEBUG", False)
 ALLOWED_HOSTS = []
 INSTALLED_APPS = [
-    "midas",
+    "midas.apps.MidasConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -29,7 +29,21 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 ROOT_URLCONF = "midas.urls"
-TEMPLATES = []
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
+]
 WSGI_APPLICATION = "midas.wsgi.application"
 DATABASES = {
     "default": {
