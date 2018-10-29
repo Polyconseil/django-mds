46a11821bb8dfb115d083ba46410601be02d7cd3
Henri <riton@melix.net>
2018-10-29 20:04:14 +0100

    [Front] Fix env variable

--- front/src/api/commons.ts
+++ front/src/api/commons.ts
@@ -1,5 +1,5 @@
 import * as urljoin from "url-join";
 
 export function buildUrl(...parts: string[]): string {
-  return urljoin(process.env.API_BASE_URL || "", ...parts);
+  return urljoin(process.env.REACT_APP_API_BASE_URL || "", ...parts);
 }
