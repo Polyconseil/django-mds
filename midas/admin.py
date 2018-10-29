d0a95fd115d3b5cac1e3d5072cef1ce27a1cb8f1
Lionel Panhaleux <lionel.panhaleux@polyconseil.fr>
2018-10-29 11:49:22 +0100

    admin: add admin for Device, Area, Service

--- midas/admin.py
+++ midas/admin.py
@@ -7,3 +7,21 @@ class QueryAdmin(admin.ModelAdmin):
     list_display = ["timestamp", "provider", "endpoint", "method"]
     list_filter = ["provider"]
     ordering = ["-timestamp"]
+
+
+@admin.register(models.Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ["provider", "technical_id", "visible_id", "model", "status"]
+    list_filter = ["provider", "model", "status"]
+
+
+@admin.register(models.Area)
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ["id", "polygons"]
+
+
+@admin.register(models.Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ["provider", "area", "begin_date", "end_date"]
+    list_filter = ["provider"]
+    ordering = ["-begin_date"]
