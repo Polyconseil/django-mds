88e20f8a559b0d84bd738c916fb411df666081b3
Sébastien Diemer <sebastien.diemer@polyconseil.fr>
2018-11-06 21:35:45 +0100

    [Front] enable loading external settings
    Useful for loading settings after the build step.

--- front/src/api/commons.ts
+++ front/src/api/commons.ts
@@ -1,9 +1,10 @@
 import * as urljoin from "url-join";
+import settings from "../settings";
 
 export function buildUrl(...parts: string[]): string {
-  let url = urljoin(process.env.REACT_APP_API_BASE_URL || "", ...parts);
+  let url = urljoin(settings.urls.apiBaseUrl, ...parts);
   if (url[url.length - 1] !== "/") {
     url = url + "/";
   }
-  return url
+  return url;
 }
---
+++ front/src/settings.tsx
@@ -0,0 +1,18 @@
+/* global __SETTINGS__ */
+
+declare var __SETTINGS__: any;
+
+const SETTINGS = {} as any;
+const envSettings = {
+  urls: {
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL
+  }
+};
+
+if (typeof __SETTINGS__ === "undefined") {
+  Object.assign(SETTINGS, envSettings);
+} else {
+  Object.assign(SETTINGS, __SETTINGS__);
+}
+
+export default SETTINGS;
