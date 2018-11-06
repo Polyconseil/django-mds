8044b7e47275c9c79bacc459b4707a9d8dd3bb58
Sébastien Diemer <sebastien.diemer@polyconseil.fr>
2018-11-06 10:41:32 +0100

    [Back] add selftest/ping

--- midas/urls.py
+++ midas/urls.py
@@ -3,6 +3,7 @@ URL Configuration
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 from rest_framework import routers
 
@@ -11,7 +12,13 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'service_area', views.ServiceAreaViewSet)
 
+
+def ok_view(request):
+    return HttpResponse("OK", status=200)
+
+
 urlpatterns = [
     url(r'^', include(router.urls)),
     path("admin/", admin.site.urls),
+    path("selftest/ping/", ok_view),
 ]
