b6e92c7576bf67a787188f971d2feb9c6fe28114
Sébastien Diemer <sebastien.diemer@polyconseil.fr>
2018-11-07 15:55:05 +0100

    [Front] Generate vehicles only once

--- front/src/api/vehicles.ts
+++ front/src/api/vehicles.ts
@@ -6,6 +6,7 @@ export interface IVehicleResponse {
   provider_id: string;
   current: {
     position: { lat: number; lng: number };
+    status: "available" | "unavailable" | "reserved";
   };
 }
 
@@ -15,6 +16,7 @@ export interface IVehicleDetailResponse {
   provider_id: string;
   current: {
     position: { lat: number; lng: number };
+    status: "available" | "unavailable" | "reserved";
   };
 }
 
@@ -34,17 +36,19 @@ function randomVehicle(): IVehicleResponse {
   if (Math.random() > 0.9) {
     return {
       current: {
-        position: randomPosition()
+        position: randomPosition(),
+        status: Math.random() > 0.3 ? "available" : Math.random() > 0.3 ? "reserved" : "unavailable",
       },
       provider_id: "bluela",
       type: "car",
-      unique_id: uuidv4()
+      unique_id: uuidv4(),
     };
   }
 
   return {
     current: {
-      position: randomPosition()
+      position: randomPosition(),
+      status: Math.random() > 0.3 ? "available" : Math.random() > 0.3 ? "reserved" : "unavailable",
     },
     provider_id: Math.random() > 0.5 ? "bird" : "lime",
     type: "scooter",
@@ -52,20 +56,24 @@ function randomVehicle(): IVehicleResponse {
   };
 }
 
+const vehiclesStore = {};
+
 export async function getVehicles(requestBody: {}): Promise<IVehiclesResponse> {
   // TODO
   // For now, mock data
 
-  const vehicles = [];
-  for (let i = 0; i < 200; i++) {
-    vehicles.push(randomVehicle());
+  if (Object.keys(vehiclesStore).length === 0) {
+    for (let i = 0; i < 200; i++) {
+      const ve = randomVehicle();
+      vehiclesStore[ve.unique_id] = ve;
+    }
   }
 
-  return Promise.resolve({ vehicles });
+  return Promise.resolve({ vehicles: Object.keys(vehiclesStore).map(key => vehiclesStore[key]) });
 }
 
 export async function getVehicleDetail(
   vehicleId: string
 ): Promise<IVehicleDetailResponse> {
-  return randomVehicle();
+  return vehiclesStore[vehicleId] || randomVehicle();
 }
