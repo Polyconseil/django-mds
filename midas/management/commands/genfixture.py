3dab4ee3a1a8cab74849b7477a1d041e3ab543ba
Lionel Panhaleux <lionel.panhaleux@polyconseil.fr>
2018-11-08 18:26:23 +0100

    factories and ladot fixture

---
+++ midas/management/commands/genfixture.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from midas import factories
+
+
+class Command(BaseCommand):
+    help = "Create a service area and 100 devices"
+
+    def handle(self, *args, **options):
+        factories.Area.create()
+        for _i in range(100):
+            factories.Device.create()
