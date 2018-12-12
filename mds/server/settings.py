452a3e699b0b9290e508d2d20a0e91f039750cfa
Sébastien Diemer <sebastien.diemer@polyconseil.fr>
2018-12-12 15:40:23 +0100

    black

--- mds/server/settings.py
+++ mds/server/settings.py
@@ -77,7 +77,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = "/static/"
-LOCALE_PATH = (os.path.join(BASE_DIR, 'locale'),)
+LOCALE_PATH = (os.path.join(BASE_DIR, "locale"),)
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
