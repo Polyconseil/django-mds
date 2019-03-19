28dfac8ab58f183f6326a357c0dc8f024f659d68
Hervé Cauwelier <github@bors-ltd.fr>
2019-03-19 16:15:44 +0100

    models: improve string representations (#60)
    This will mostly serve in the Django admin.
    
    Can't find anything relevant for EventRecord, without accessing related
    device or provider.

--- mds/models.py
+++ mds/models.py
@@ -60,6 +60,11 @@ def api_configuration_default():
     return {"trailing_slash": False}  # Some providers endpoint won't reply without it
 
 
+def short_uuid4(uid):
+    """When seeing a glimpse of the UID is enough."""
+    return str(uid)[:8]  # Basically splitting on the first dash
+
+
 class Provider(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = UnboundedCharField(default="")
@@ -87,6 +92,9 @@ class Provider(models.Model):
         default=agency_api_authentication_default,
         verbose_name="Agency API Authentication",
     )
+
+    def __str__(self):
+        return "{} ({})".format(self.name, short_uuid4(self.id))
 
 
 class DeviceQueryset(models.QuerySet):
@@ -136,6 +144,13 @@ class Device(models.Model):
     )
 
     objects = DeviceQueryset.as_manager()
+
+    def __str__(self):
+        return "{} {} ({})".format(
+            self.get_category_display(),
+            self.identification_number,
+            short_uuid4(self.id),
+        )
 
     @property
     def latest_event(self):
@@ -198,6 +213,9 @@ class Polygon(models.Model):
     geom = gis_models.PolygonField()
     properties = pg_fields.JSONField(default=dict, encoder=encoders.JSONEncoder)
 
+    def __str__(self):
+        return "{} ({})".format(self.label, short_uuid4(self.id))
+
 
 class Area(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
@@ -214,3 +232,6 @@ class Area(models.Model):
     area_type = UnboundedCharField(
         choices=enums.choices(enums.AREA_TYPE), default="unrestricted"
     )
+
+    def __str__(self):
+        return "{} ({})".format(self.label, short_uuid4(self.id))
