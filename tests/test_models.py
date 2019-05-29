bee85bc9be03f69ed12657d9b50d98a84a03aed6
Hervé Cauwelier <herve.cauwelier@polyconseil.fr>
2019-05-29 14:42:27 +0200

    tests: build to get the expected result
    This was an accident waiting to happen.

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,10 +6,9 @@ from mds import models
 
 @pytest.mark.django_db
 def test_with_device_categories(admin_client):
-    factories.Device.create_batch(10, provider=factories.Provider())
+    provider = factories.Provider()
+    factories.Device.create_batch(3, category="bicycle", provider=provider)
+    factories.Device.create_batch(2, category="scooter", provider=provider)
+    factories.Device.create_batch(1, category="car", provider=provider)
     provider = models.Provider.objects.with_device_categories().get()
-    assert sum(provider.device_categories.values()) == {
-        "bicycle": 3,
-        "scooter": 3,
-        "car": 4,
-    }
+    assert provider.device_categories == {"bicycle": 3, "scooter": 2, "car": 1}
