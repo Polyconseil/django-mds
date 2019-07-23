fce671380cd7f7b08463d317efcf0632240de751
Antoine Garcia <a.garcia.walecha@gmail.com>
2019-07-23 10:54:01 +0200

    Fix crash on event record admin panel (#133)

--- mds/server/settings.py
+++ mds/server/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
+    "django.contrib.gis",
     "django.contrib.contenttypes",
     "django.contrib.messages",
     "django.contrib.postgres",
